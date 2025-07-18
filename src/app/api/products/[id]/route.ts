adccd3599920e62758173d68fd3a68f9f52204d8
aitezazdev <aitezazsikandar@gmail.com>
2025-07-18 12:29:27 +0500

    add validation to put request for products

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,7 +1,7 @@
 import { connectDB } from "@/lib/db";
 import { handleApiError } from "@/lib/middlewares/error";
 import { Product } from "@/lib/models/Product";
-import { productSchema } from "@/lib/validations/product";
+import { partialProductSchema } from "@/lib/validations/product";
 import { validateRequest } from "@/lib/validations/validateSchema";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -18,26 +18,40 @@ export const PUT = async (req: NextRequest, { params }: Params) => {
 
     const { id } = params;
 
-    const validatedData = await validateRequest(productSchema, req);
-    if (validatedData instanceof NextResponse) return validatedData;
-
-    const { title, description, category, price, image, size } = validatedData;
-
-    if (!title || !description || !category || !price || !size || !image) {
+    const product = await Product.findById(id);
+    if (!product) {
       return NextResponse.json(
-        { message: "All fields are required" },
-        { status: 400 }
+        {
+          message: "Product with the id not found",
+        },
+        { status: 404 }
       );
     }
 
-    const updateProduct = await Product.findByIdAndUpdate(id, {
-      title,
-      description,
-      category,
-      price,
-      size,
-      image,
-    });
+    const validatedData = await validateRequest(partialProductSchema, req);
+    if (validatedData instanceof NextResponse) return validatedData;
+
+    const {
+      title = product.title,
+      description = product.description,
+      category = product.category,
+      price = product.price,
+      size = product.size,
+      image = product.image,
+    } = validatedData;
+
+    const updateProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        category,
+        price,
+        size,
+        image,
+      },
+      { new: true }
+    );
     if (!updateProduct) {
       return NextResponse.json(
         { message: "Product not found" },
